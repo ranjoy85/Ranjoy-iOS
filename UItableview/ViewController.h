81eafb2fdb335aad8a98ebd46446500b9164779c
ranjoy <ranjoy@ranjoys-Mac.local>
2013-12-07 22:02:06 -0800

    Initial Commit

---
+++ UItableview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UItableview
+//
+//  Created by ranjoy on 12/7/13.
+//  Copyright (c) 2013 ranjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ UItableview/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UItableview
+//
+//  Created by ranjoy on 12/7/13.
+//  Copyright (c) 2013 ranjoy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
